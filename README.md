e3ece380a164c8f89b30a10f191487c1ac822240
Noritaka IZUMI <noritaka.izumi@gmail.com>
2020-05-17 06:42:09 +0900

    Electron app 生成手順を追加．

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 `index.html` をブラウザで開くとすぐに遊ぶことができます．
 
+## Electron でアプリを生成する
+
+Windows の場合
+
+```shell script
+npm i -D electron
+npm i -D electron-packager
+cd /path/to/flash_anzan
+npx electron-packager src flash_anzan --platform=win32 --arch=x64 --overwrite
+```
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const limits = {
     digit: {
-        upper: 8,
+        upper: 14,
         lower: 1,
     },
     length: {
@@ -40,8 +40,17 @@ function increaseParam(id, amount) {
     }
 }
 
+const answerTitle = document.getElementById("answer-title");
+const numberArea = document.getElementById("number-area");
+
+const startButton = document.getElementById("start-button");
 const answerButton = document.getElementById("answer-button");
-const answerArea = document.getElementById("answer-area");
+const repeatButton = document.getElementById("repeat-button");
+
+const disableConfigTarget = Array.from(document.getElementsByClassName("disable-config-target"));
+
+const resultSection = document.getElementById("result-section");
+const answerNumber = document.getElementById("answer-number");
 const numberHistoryArea = document.getElementById("number-history-area");
 
 const soundDirectory = "./sound";
@@ -52,17 +61,24 @@ const answerSoundUrl = soundDirectory + "/answer.wav";
 function flash(config = {}) {
     const startButton = document.getElementById("start-button");
     const repeatButton = document.getElementById("repeat-button");
-    const numberArea = document.getElementById("number-area");
 
     const numberHistoryDisplay = document.getElementById("number-history-display");
     const numberHistoryDisplayDelimiter = "\n";
     const numberHistoryString = document.getElementById("number-history-stringify");
     const numberHistoryStringifyDelimiter = "|";
 
-    const digit = fixValue(limits.digit, Math.floor(Number(document.getElementById("digit").value)));
-    const length = fixValue(limits.length, Math.floor(Number(document.getElementById("length").value)));
-    const time = fixValue(limits.time, Number(document.getElementById("time").value) * 1000);
-    const flashRate = fixValue(limits.flashRate, Number(document.getElementById("flash-rate").value));
+    const digitElement = document.getElementById("digit");
+    const digitParam = fixValue(limits.digit, Math.floor(Number(digitElement.value)));
+    digitElement.value = digitParam;
+    const lengthElement = document.getElementById("length");
+    const lengthParam = fixValue(limits.length, Math.floor(Number(lengthElement.value)));
+    lengthElement.value = lengthParam;
+    const timeElement = document.getElementById("time");
+    const timeMsParam = fixValue(limits.time, Number(timeElement.value) * 1000);
+    timeElement.value = timeMsParam / 1000;
+    const flashRateElement = document.getElementById("flash-rate");
+    const flashRateParam = fixValue(limits.flashRate, Number(flashRateElement.value));
+    flashRateElement.value = flashRateParam;
 
     function getFlashTime(length, time, flashRate) {
         const averageFlashTime = time / (length * 2);
@@ -71,7 +87,8 @@ function flash(config = {}) {
         return {on: flashOnTime, off: flashOffTime};
     }
 
-    const flashTime = getFlashTime(length, time, flashRate);
+    // 点灯時間と消灯時間を算出する
+    const flashTime = getFlashTime(lengthParam, timeMsParam, flashRateParam);
     const flashOnTime = flashTime.on;
     const flashOffTime = flashTime.off;
 
@@ -88,56 +105,61 @@ function flash(config = {}) {
             const max = Math.pow(10, digit) - 1;
             numbers.push(getRandomInt(min, max));
         }
-        numbers.push("");
         return numbers;
+    }
+
+    function generateToggleNumberSuite(numbers) {
+        let toggleNumberSuite = [];
+        for (let i = 0; i < numbers.length; i++) {
+            toggleNumberSuite.push(numbers[i]);
+            toggleNumberSuite.push("");
+        }
+        return toggleNumberSuite;
     }
 
     function generateSounds() {
         let sounds = [];
-        for (let i = 0; i < length; ++i) {
+        for (let i = 0; i < lengthParam; ++i) {
             sounds.push(new Audio(tickSoundUrl));
+            sounds.push(new Audio());
         }
-        sounds.push(new Audio());
         return sounds;
     }
 
     let numbers;
-    if (config.repeat) {
-        numbers = numberHistoryString.innerText.split(numberHistoryStringifyDelimiter).map((n) => n ? Number(n) : "");
-    } else {
-        numbers = generateNumbers(digit, length);
-    }
-    const localeStringNumbers = numbers.map((n) => n.toLocaleString());
-    const sounds = generateSounds();
-    let index = 0;
-    let flag = 0;
-
-    function toggleNumber() {
-        if (index >= localeStringNumbers.length) {
-            return;
-        }
-
-        if (flag === 0) {
-            numberArea.innerText = localeStringNumbers[index];
-            sounds[index].play();
-            index++;
-            flag = 1;
-            setTimeout(toggleNumber, flashOnTime);
+    const numberHistory = numberHistoryString.innerText.split(numberHistoryStringifyDelimiter).map((n) => n ? Number(n) : "");
+    if (config.repeat && digitParam === numberHistory[0].toString().length) {
+        if (lengthParam === numberHistory.length) {
+            numbers = numberHistory;
+        } else if (lengthParam < numberHistory.length) {
+            numbers = numberHistory.slice(0, lengthParam);
         } else {
-            numberArea.innerText = "";
-            flag = 0;
-            setTimeout(toggleNumber, flashOffTime);
+            numbers = numberHistory.concat(generateNumbers(digitParam, lengthParam - numberHistory.length))
         }
+    } else {
+        numbers = generateNumbers(digitParam, lengthParam);
+    }
+    console.log(numbers); // わざと出力している
+    const localeStringNumbers = numbers.map((n) => n.toLocaleString());
+    const toggleNumberSuite = generateToggleNumberSuite(localeStringNumbers);
+    const soundSuite = generateSounds();
+    let index = 0;
+
+    // setTimeout に登録するので，引数無しで実装する
+    function toggleNumber() {
+        numberArea.innerText = toggleNumberSuite[index];
+        soundSuite[index].play();
+        index++;
     }
 
     function disableButtons() {
         startButton.disabled = true;
         repeatButton.disabled = true;
         answerButton.disabled = true;
+        disableConfigTarget.map((element) => element.disabled = true);
     }
 
     function enableButtons() {
-        startButton.disabled = false;
         repeatButton.disabled = false;
         answerButton.disabled = false;
     }
@@ -153,31 +175,43 @@ function flash(config = {}) {
         beep2.play().then(r => r);
     }
 
+    // Register flash events
     const offset = 500;
     const beepInterval = 875;
     const flashStartTiming = offset + beepInterval * 2;
     setTimeout(disableButtons, 0);
     setTimeout(playBeep1, offset);
     setTimeout(playBeep2, offset + beepInterval);
+    // 1 回目の表示が遅延することがあるので，ウォーミングアップで 1 回見えない文字を光らせておく
     setTimeout(() => numberArea.style.color = "black", flashStartTiming - 100);
     setTimeout(() => numberArea.innerText = "0", flashStartTiming - 75);
     setTimeout(() => numberArea.innerText = "", flashStartTiming - 50);
     setTimeout(() => numberArea.style.color = "limegreen", flashStartTiming - 25);
-    setTimeout(toggleNumber, flashStartTiming);
-    setTimeout(enableButtons, flashStartTiming + time);
+    let toggleTiming = flashStartTiming;
+    for (let i = 0; i < localeStringNumbers.length; i++) {
+        setTimeout(toggleNumber, toggleTiming);
+        toggleTiming += flashOnTime;
+        setTimeout(toggleNumber, toggleTiming);
+        toggleTiming += flashOffTime;
+    }
+    setTimeout(enableButtons, flashStartTiming + timeMsParam);
 
+    answerTitle.style.display = "none";
     numberArea.innerText = "";
-    answerArea.style.display = "none";
+    resultSection.style.display = "none";
     numberHistoryArea.style.display = "none";
-    const sum = Number(numbers.reduce((a, b) => a + b));
-    document.getElementById("sum").innerText = sum.toLocaleString();
+    answerNumber.innerText = Number(numbers.reduce((a, b) => a + b)).toLocaleString();
     numberHistoryDisplay.innerText = localeStringNumbers.join(numberHistoryDisplayDelimiter);
     numberHistoryString.innerText = numbers.join(numberHistoryStringifyDelimiter);
 }
 
 function displayAnswer() {
+    startButton.disabled = false;
+    disableConfigTarget.map((element) => element.disabled = false);
     answerButton.disabled = true;
-    answerArea.style.display = "block";
+    answerTitle.style.display = "block";
+    // resultSection.style.display = "block";
+    numberArea.innerText = answerNumber.innerText;
     new Audio(answerSoundUrl).play().then(r => r);
 }
 
@@ -188,3 +222,8 @@ function toggleNumberHistoryArea() {
         numberHistoryArea.style.display = "block";
     }
 }
+
+// Shortcuts
+shortcut.add("s", () => startButton.click());
+shortcut.add("a", () => answerButton.click());
+shortcut.add("r", () => repeatButton.click());
