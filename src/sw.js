55f33bce6143c3ec14bb2b547c3804767e257c47
Noritaka IZUMI <noritaka.izumi@gmail.com>
2022-01-25 09:57:22 +0900

    fix: service worker のエラー直した

--- src/sw.js
+++ src/sw.js
@@ -29,6 +29,9 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('fetch', function(event) {
+    if (event.request.url.indexOf('http') !== 0) {
+        return;
+    }
     event.respondWith(
         caches.match(event.request)
             .then(function(response) {
