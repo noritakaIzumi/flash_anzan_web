908b0dc576329dca8e0bd2156bcc835dde7f989b
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-09-02 20:31:26 +0900

    update: ビルドファイル名にハッシュを付けないようにする

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,14 @@ const path = require('path')
 export default defineConfig({
     root: path.resolve(__dirname, 'src'),
     build: {
-        outDir: '../dist'
+        outDir: '../dist',
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name].js',
+                assetFileNames: 'assets/[name].[ext]',
+            },
+        }
     },
     css: {
         devSourcemap: true,
