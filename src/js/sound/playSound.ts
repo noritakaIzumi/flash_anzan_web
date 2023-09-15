ccaa29b483e57da42ee2c1eec432d315da7e9b39
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-09-16 06:53:54 +0900

    update: フラッシュ出題音声で明示的な無音の読み込みをやめる

--- src/js/sound/playSound.ts
+++ src/js/sound/playSound.ts
@@ -2,7 +2,7 @@ import { type SoundExtension } from "./sound.js"
 import { audioAttr } from "../globals.js"
 import { getCrunkerInstance } from "./crunker.js"
 
-const audioBuffersCollection: { [key in SoundExtension]: AudioBuffer[] | undefined } = {
+const audioBuffersCollection: { [key in SoundExtension]: Array<[AudioBuffer, AudioBuffer]> | undefined } = {
     ogg: undefined,
     wav: undefined,
 }
@@ -11,15 +11,14 @@ export async function initAudioBuffers(extension: SoundExtension): Promise<void>
     if (audioBuffersCollection[extension] === undefined) {
         audioBuffersCollection[extension] = await getCrunkerInstance().fetchAudio(
             `${audioAttr.directory}/beep.${extension}`,
-            `${audioAttr.directory}/tick.${extension}`,
-            `${audioAttr.directory}/silence.${extension}`
+            `${audioAttr.directory}/tick.${extension}`
         )
     }
 }
 
-async function getAudioBuffers(extension: SoundExtension): Promise<AudioBuffer[]> {
+async function getAudioBuffers(extension: SoundExtension): Promise<Array<[AudioBuffer, AudioBuffer]>> {
     await initAudioBuffers(extension)
-    return audioBuffersCollection[extension] as AudioBuffer[]
+    return audioBuffersCollection[extension] as Array<[AudioBuffer, AudioBuffer]>
 }
 
 export class PlaySoundCreator {
@@ -36,14 +35,16 @@ export class PlaySoundCreator {
         beepInterval: number
         toggleTimings: number[]
     }): Promise<PlaySound> {
-        const [beepAudioBuffer, tickAudioBuffer, silenceAudioBuffer]: AudioBuffer[] = await getAudioBuffers(props.extension)
+        const [beepAudioBuffer, tickAudioBuffer]: Array<[AudioBuffer, AudioBuffer]> = await getAudioBuffers(props.extension)
 
         const audios: AudioBuffer[] = []
         audios.push(this.crunker.padAudio(beepAudioBuffer, 0, 0))
         audios.push(this.crunker.padAudio(beepAudioBuffer, 0, props.beepInterval / 1000))
         const beepStart = props.beepInterval * 2 / 1000
         for (const [i, toggleTiming] of props.toggleTimings.entries()) {
-            audios.push(this.crunker.padAudio(i % 2 === 0 ? tickAudioBuffer : silenceAudioBuffer, 0, beepStart + toggleTiming / 1000))
+            if (i % 2 === 0) {
+                audios.push(this.crunker.padAudio(tickAudioBuffer, 0, beepStart + toggleTiming / 1000))
+            }
         }
 
         return new PlaySound(this.crunker.mergeAudio(audios))
