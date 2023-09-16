c8a1987b84c7619aa89c9eb332a116b58b74bba2
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-09-16 20:33:16 +0900

    update: audiosprite をメイン処理に組み込む

--- src/js/sound/sound.ts
+++ src/js/sound/sound.ts
@@ -1,39 +1,46 @@
-import { audioAttr, audioObjKey, type AudioObjKey, type AudioPath, type SoundExtension } from "../globals.js"
-import { Howl } from "howler"
+import { type AudioObjKey, type SoundExtension } from "../globals.js"
+import { Howl, type HowlOptions } from "howler"
 import { flashParamElements } from "../dom/flashParamElements.js"
 import { initAudioBuffers } from "./playSound.js"
+import { howler2OptionWav } from "../lib/howler2OptionWav.js"
+import { howler2OptionOgg } from "../lib/howler2OptionOgg.js"
 
 export function isMuted(): boolean {
     return flashParamElements.common.isMuted.valueV1
 }
 
+const howlStore: { [ext in SoundExtension]?: Howl | undefined } = {}
+const howlOptions: { [ext in SoundExtension]: HowlOptions } = {
+    ogg: howler2OptionOgg,
+    wav: howler2OptionWav,
+}
+
+function getHowl(extension: SoundExtension): Howl {
+    if (howlStore[extension] === undefined) {
+        howlStore[extension] = new Howl(howlOptions[extension])
+    }
+    return howlStore[extension] as Howl
+}
+
 class AudioObj {
-    private readonly beep: Howl[] = new Array(0)
-    private readonly tick: Howl[] = new Array(0)
-    private readonly answer: Howl[] = new Array(1)
-    private readonly correct: Howl[] = new Array(1)
-    private readonly incorrect: Howl[] = new Array(1)
-    private readonly silence: Howl[] = new Array(1)
+    private currentHowl: Howl | undefined
 
     load(extension: SoundExtension): void {
-        let audioPath: AudioPath
-        audioObjKey.forEach(name => {
-            audioPath = `${audioAttr.directory}/${name}.${extension}`
-            for (let i = 0; i < this[name].length; i++) {
-                this[name][i] = new Howl({ src: [audioPath] })
-            }
-        })
+        this.currentHowl = getHowl(extension)
         void initAudioBuffers(extension, "beep")
         void initAudioBuffers(extension, "tick")
     }
 
     play(name: AudioObjKey): void {
+        if (this.currentHowl === undefined) {
+            throw new Error("audio is not initialized")
+        }
         switch (name) {
             case "answer":
             case "correct":
             case "incorrect":
             case "silence":
-                this[name][0].play()
+                this.currentHowl.play(name)
                 break
             default:
                 throw new Error(`The sound "${name}" cannot play directly`)
