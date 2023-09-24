8a053b56cf9c0d3aaa894f72d5283e63ffcb8ba0
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-09-24 18:07:24 +0900

    build: フラッシュ複雑度サンプルデータを中間データとして書き出し、利用できるようにする

--- tools/createComplexityThresholdMap.ts
+++ tools/createComplexityThresholdMap.ts
@@ -1,5 +1,10 @@
 import { calculateComplexity } from "../src/js/flash/flashAnalysis.js"
-import { type ComplexityThreshold, type ComplexityThresholdMap, type FlashMode } from "../src/js/globals.js"
+import {
+    type ComplexityThreshold,
+    type ComplexityThresholdMap,
+    type ComplexityThresholdMapKey,
+    type FlashMode
+} from "../src/js/globals.js"
 import {
     AdditionModeUnknownDifficultyCreateRawNumbersAdapter,
     MultiplicationModeUnknownDifficultyCreateRawNumbersAdapter
@@ -7,8 +12,11 @@ import {
 import * as fs from "fs"
 import * as path from "path"
 import { fileURLToPath } from "url"
-
 import { flashParamSchema } from "../src/config/flashParamSchema.js"
+import { eslintFix } from "./eslintFix.js"
+
+const filename = fileURLToPath(import.meta.url)
+const complexitySampleMapDir = path.dirname(path.dirname(filename)) + "/data/complexitySampleMap"
 
 function getRank(numbers: number[], threshold: Threshold): ComplexityThreshold {
     const half = (numbers.length / 2) | 0
@@ -26,12 +34,36 @@ interface Threshold {
     easy: number
 }
 
+type ComplexitySampleMap = { [key1 in FlashMode]: { [key2 in ComplexityThresholdMapKey[key1]]: number[] } }
+
+function complexitySampleFilepath<T extends FlashMode>(mode: T, mapKey: ComplexityThresholdMapKey[T]): string {
+    return `${complexitySampleMapDir}/${mode}/${mapKey}.json`
+}
+
+function readComplexitySample<T extends FlashMode>(mode: T, mapKey: ComplexityThresholdMapKey[T]): number[] {
+    const filepath = complexitySampleFilepath(mode, mapKey)
+    return JSON.parse(fs.readFileSync(filepath, "utf-8"))
+}
+
+function writeComplexitySample<T extends FlashMode>(mode: T, mapKey: ComplexityThresholdMapKey[T], sample: number[]): void {
+    const filepath = complexitySampleFilepath(mode, mapKey)
+    fs.writeFileSync(filepath, JSON.stringify(sample, undefined, 2))
+}
+
 (() => {
     // main
+    const importComplexitySampleMapFromFile: boolean = process.argv[2] === "reuse"
+    const complexityThresholdMapFilepath = path.dirname(path.dirname(filename)) + "/src/js/lib/complexityThresholdMap.ts"
+
+    const complexitySampleMap: ComplexitySampleMap = {
+        addition: {},
+        multiplication: {},
+    }
     const complexityThresholdMap: ComplexityThresholdMap = {
         addition: {},
         multiplication: {},
     }
+
     const sampleCount = 10000
     const threshold: Threshold = {
         hard: 0.1,
@@ -39,48 +71,72 @@ interface Threshold {
     }
 
     let mode: FlashMode = "addition"
+    if (!importComplexitySampleMapFromFile) {
+        fs.mkdirSync(`${complexitySampleMapDir}/${mode}`, { recursive: true })
+    }
     const additionModeParamSchema = flashParamSchema.addition
     for (let digitCount = additionModeParamSchema.digit.min; digitCount <= additionModeParamSchema.digit.difficultySupportMax; ++digitCount) {
         for (let length = additionModeParamSchema.length.min; length <= additionModeParamSchema.length.difficultySupportMax; ++length) {
-            const complexities: number[] = []
-            for (let _ = 0; _ < sampleCount; _++) {
-                const rawNumbers = new AdditionModeUnknownDifficultyCreateRawNumbersAdapter().execute(digitCount, length)
-                const complexity = calculateComplexity(rawNumbers.carries, digitCount)
-                complexities.push(complexity)
+            const mapKey: ComplexityThresholdMapKey["addition"] = `${digitCount}-${length}`
+            if (importComplexitySampleMapFromFile) {
+                complexitySampleMap[mode][mapKey] = readComplexitySample("addition", mapKey)
+            } else {
+                complexitySampleMap[mode][mapKey] = []
+                for (let _ = 0; _ < sampleCount; _++) {
+                    const rawNumbers = new AdditionModeUnknownDifficultyCreateRawNumbersAdapter().execute(digitCount, length)
+                    const complexity = calculateComplexity(rawNumbers.carries, digitCount)
+                    complexitySampleMap[mode][mapKey].push(complexity)
+                }
+                writeComplexitySample(mode, mapKey, complexitySampleMap[mode][mapKey])
             }
-            complexityThresholdMap.addition[`${digitCount}-${length}`] = getRank(complexities, threshold)
+            complexityThresholdMap.addition[mapKey] = getRank(complexitySampleMap.addition[mapKey], threshold)
             console.log(`${mode} ${digitCount} 桁 ${length} 口: completed`)
         }
     }
 
     mode = "multiplication"
+    if (!importComplexitySampleMapFromFile) {
+        fs.mkdirSync(`${complexitySampleMapDir}/${mode}`, { recursive: true })
+    }
     const multiplicationModeParamSchema = flashParamSchema.multiplication
     for (let digitCount1 = multiplicationModeParamSchema.digit1.min; digitCount1 <= multiplicationModeParamSchema.digit1.difficultySupportMax; digitCount1++) {
         for (let digitCount2 = multiplicationModeParamSchema.digit2.min; digitCount2 <= multiplicationModeParamSchema.digit2.difficultySupportMax; digitCount2++) {
             for (let length = multiplicationModeParamSchema.length.min; length <= multiplicationModeParamSchema.length.difficultySupportMax; length++) {
-                const digitFlippedComplexityThreshold: ComplexityThreshold | undefined = complexityThresholdMap.multiplication[`${digitCount2}-${digitCount1}-${length}`]
-                if (digitFlippedComplexityThreshold === undefined) {
-                    complexityThresholdMap.multiplication[`${digitCount1}-${digitCount2}-${length}`] = digitFlippedComplexityThreshold
+                const mapKey: ComplexityThresholdMapKey["multiplication"] = `${digitCount1}-${digitCount2}-${length}`
+                const flippedMapKey: ComplexityThresholdMapKey["multiplication"] = `${digitCount2}-${digitCount1}-${length}`
+                if (flippedMapKey in complexityThresholdMap.multiplication) {
+                    complexitySampleMap[mode][mapKey] = complexitySampleMap[mode][flippedMapKey]
+                    complexityThresholdMap[mode][mapKey] = complexityThresholdMap[mode][flippedMapKey]
                     console.log(`${mode} ${digitCount1} 桁× ${digitCount2} 桁 ${length} 口: ${digitCount2} 桁× ${digitCount1} 桁 ${length} 口 から copy`)
                     continue
                 }
-                const complexities: number[] = []
-                for (let _ = 0; _ < sampleCount; _++) {
-                    const rawNumbers = new MultiplicationModeUnknownDifficultyCreateRawNumbersAdapter().execute([digitCount1, digitCount2], length)
-                    const complexity = calculateComplexity(rawNumbers.carries, digitCount1 * digitCount2)
-                    complexities.push(complexity)
+                if (importComplexitySampleMapFromFile) {
+                    complexitySampleMap[mode][mapKey] = readComplexitySample("multiplication", mapKey)
+                } else {
+                    complexitySampleMap[mode][mapKey] = []
+                    for (let _ = 0; _ < sampleCount; _++) {
+                        const rawNumbers = new MultiplicationModeUnknownDifficultyCreateRawNumbersAdapter().execute([digitCount1, digitCount2], length)
+                        const complexity = calculateComplexity(rawNumbers.carries, digitCount1 * digitCount2)
+                        complexitySampleMap[mode][mapKey].push(complexity)
+                    }
+                    writeComplexitySample(mode, mapKey, complexitySampleMap[mode][mapKey])
                 }
-                complexityThresholdMap.multiplication[`${digitCount1}-${digitCount2}-${length}`] = getRank(complexities, threshold)
+                complexityThresholdMap.multiplication[mapKey] = getRank(complexitySampleMap.multiplication[mapKey], threshold)
                 console.log(`${mode} ${digitCount1} 桁× ${digitCount2} 桁 ${length} 口: completed`)
             }
         }
     }
 
     try {
-        const filename = fileURLToPath(import.meta.url)
         fs.writeFileSync(
-            path.dirname(path.dirname(filename)) + "/src/js/lib/complexityThresholdMap.ts",
-            `import{ComplexityThresholdMap}from"../globals.js";export const complexityThresholdMap:ComplexityThresholdMap=${JSON.stringify(complexityThresholdMap)};\n`)
+            complexityThresholdMapFilepath,
+            `
+import { ComplexityThresholdMap } from "../globals.js"
+
+export const complexityThresholdMap: ComplexityThresholdMap = ${JSON.stringify(complexityThresholdMap, undefined, 4)};
+`.trim()
+        )
+        eslintFix(complexityThresholdMapFilepath)
         console.log("write end")
     } catch (e) {
         console.log(e)
