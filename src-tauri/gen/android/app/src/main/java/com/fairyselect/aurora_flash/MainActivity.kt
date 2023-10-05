1610ca3fef39fe0093d6b27d2d8b1beb5d9dbac0
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-10-05 09:02:18 +0900

    build: add tauri android app build settings

---
+++ src-tauri/gen/android/app/src/main/java/com/fairyselect/aurora_flash/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.fairyselect.aurora_flash
+
+class MainActivity : TauriActivity()
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type UserConfig } from 'vite'
+import { internalIpV4 } from 'internal-ip'
 import * as path from 'path'
 import * as fs from 'fs'
 import { createHtmlPlugin } from 'vite-plugin-html'
@@ -11,59 +12,71 @@ const appVersion = (() => {
 
 const [appName, appDescription] = (() => {
     const json = fs.readFileSync(path.resolve(__dirname) + '/src/public/manifest.json')
-    const body = JSON.parse(json.toString()) as { name: string, description: string }
+    const body = JSON.parse(json.toString()) as { name: string; description: string }
     return [body.name, body.description]
 })()
 
 // noinspection JSUnusedGlobalSymbols
-export default defineConfig({
-    root: path.resolve(__dirname, 'src'),
-    base: '',
-    build: {
-        outDir: '../dist',
-        rollupOptions: {
-            output: {
-                entryFileNames: 'assets/[name].[hash].js',
-                chunkFileNames: 'assets/[name].[hash].js',
-                assetFileNames: 'assets/[name].[hash].[ext]',
-            },
-        },
-        // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-        target: process.env.TAURI_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
-        // don't minify for debug builds
-        minify: (process.env.TAURI_DEBUG == null) ? 'esbuild' : false,
-        // produce sourcemaps for debug builds
-        sourcemap: !(process.env.TAURI_DEBUG == null),
-    },
-    // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
-    // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
-    // env variables
-    envPrefix: ['VITE_', 'TAURI_'],
-    define: {
-        APP_CONFIG_WAIT_SOUNDS_AND_FONTS_LOADED_TIMEOUT: 30000,
-    },
-    plugins: [
-        createHtmlPlugin({
-            minify: true,
-            inject: {
-                data: {
-                    title: appName,
-                    description: appDescription,
-                    versionNumber: appVersion,
+export default defineConfig(async () => {
+    const host = await internalIpV4()
+
+    const config: UserConfig = {
+        root: path.resolve(__dirname, 'src'),
+        base: '',
+        build: {
+            outDir: '../dist',
+            rollupOptions: {
+                output: {
+                    entryFileNames: 'assets/[name].[hash].js',
+                    chunkFileNames: 'assets/[name].[hash].js',
+                    assetFileNames: 'assets/[name].[hash].[ext]',
                 },
             },
-        }),
-    ],
-    css: {
-        devSourcemap: true,
-    },
-    server: {
-        port: 8080,
-        watch: {
-            usePolling: false,
+            // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+            target: process.env.TAURI_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
+            // don't minify for debug builds
+            minify: process.env.TAURI_DEBUG == null ? 'esbuild' : false,
+            // produce sourcemaps for debug builds
+            sourcemap: !(process.env.TAURI_DEBUG == null),
         },
-        strictPort: true, // Tauri expects a fixed port, fail if that port is not available
-    },
-    // prevent vite from obscuring rust errors
-    clearScreen: false,
+        // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
+        // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
+        // env variables
+        envPrefix: ['VITE_', 'TAURI_'],
+        define: {
+            APP_CONFIG_WAIT_SOUNDS_AND_FONTS_LOADED_TIMEOUT: 30000,
+        },
+        plugins: [
+            createHtmlPlugin({
+                minify: true,
+                inject: {
+                    data: {
+                        title: appName,
+                        description: appDescription,
+                        versionNumber: appVersion,
+                    },
+                },
+            }),
+        ],
+        css: {
+            devSourcemap: true,
+        },
+        server: {
+            host: '0.0.0.0',
+            port: 8080,
+            strictPort: true,
+            hmr: {
+                protocol: 'ws',
+                host,
+                port: 5183,
+            },
+            watch: {
+                usePolling: false,
+            },
+        },
+        // prevent vite from obscuring rust errors
+        clearScreen: false,
+    }
+
+    return config
 })
