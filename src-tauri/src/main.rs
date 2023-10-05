09f8328cfae33a48aa4a989dc18d4ad355e90c05
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-10-06 07:39:51 +0900

    build: introduce conditional compilation

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,21 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
 
 pub fn main() {
+  run_app()
+}
+
+#[cfg(mobile)]
+fn run_app() {
   app::AppBuilder::new().run();
+}
+
+#[cfg(desktop)]
+fn run_app() {
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
