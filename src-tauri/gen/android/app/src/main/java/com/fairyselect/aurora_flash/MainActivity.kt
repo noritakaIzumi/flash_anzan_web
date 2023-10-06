b366b4a5777848f4663654f9abd66639fb7ff36a
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-10-07 07:41:19 +0900

    fix: モバイルでステータスバーを非表示にしない

--- src-tauri/gen/android/app/src/main/java/com/fairyselect/aurora_flash/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/fairyselect/aurora_flash/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : TauriActivity() {
         super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.decorView.windowInsetsController?.apply {
-                hide(WindowInsets.Type.systemBars())
+                hide(WindowInsets.Type.statusBars())
                 systemBarsBehavior = BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             }
         } else {
