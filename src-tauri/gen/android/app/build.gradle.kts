dd0a06933eb1f6045e84b20911f5f4ea90f0fcd0
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-10-09 07:51:18 +0900

    chore: update version to 0.38.2

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.fairyselect.aurora_flash"
         minSdk = 24
         targetSdk = 33
-        versionCode = 5
-        versionName = "0.38.1"
+        versionCode = 6
+        versionName = "0.38.2"
     }
     signingConfigs {
         create("release") {
