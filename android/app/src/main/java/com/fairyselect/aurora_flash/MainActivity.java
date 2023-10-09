43223650048346b6dd65e26cd731f477efe25658
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-10-09 18:15:30 +0900

    update: add android files of capacitor

---
+++ android/app/src/main/java/com/fairyselect/aurora_flash/MainActivity.java
@@ -0,0 +1,5 @@
+package com.fairyselect.aurora_flash;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { type CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+    appId: 'com.fairyselect.aurora_flash',
+    appName: 'aurora-flash',
+    webDir: 'dist',
+    server: {
+        androidScheme: 'https',
+    },
+}
+
+export default config
