81a52b8cc4fe72fc8f5e1e735739e6b20829ac6f
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-10-29 12:45:05 +0900

    build!: tauri 版 android ソースを削除

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-pub fn main() {
-  run_app()
-}
-
-#[cfg(mobile)]
-fn run_app() {
-  app::AppBuilder::new().run();
-}
-
-#[cfg(desktop)]
-fn run_app() {
+fn main() {
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
